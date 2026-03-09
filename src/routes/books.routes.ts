ce7f235de80e4f472071a85f64ea233b7234d548
dnii <123633815+danny-oko@users.noreply.github.com>
2026-03-09 11:34:38 +0800

    small change

--- src/routes/books.routes.ts
+++ src/routes/books.routes.ts
@@ -4,8 +4,7 @@ import { createBook } from "../controllers/books/post-books";
 import { getBooks } from "../controllers/books/get-books";
 
 export const registerFoodRoutes = (app: Hono) => {
-
   app.get("/books", getBooks);
   app.get("/books/:id", getBooksById);
-  app.post("/books", createBook); 
+  app.post("/books", createBook);
 };
