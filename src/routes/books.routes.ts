9534045733f43ae430a8c1f9b5726b9d825d860c
dnii <123633815+danny-oko@users.noreply.github.com>
2026-03-09 11:44:24 +0800

    put

--- src/routes/books.routes.ts
+++ src/routes/books.routes.ts
@@ -2,9 +2,11 @@ import { Hono, HonoRequest } from "hono";
 import { getBooksById } from "../controllers/books/get-books-by-id";
 import { createBook } from "../controllers/books/post-books";
 import { getBooks } from "../controllers/books/get-books";
+import { updatedBooks } from "../controllers/books/put-books-by-id";
 
 export const registerFoodRoutes = (app: Hono) => {
   app.get("/books", getBooks);
   app.get("/books/:id", getBooksById);
   app.post("/books", createBook);
+  app.put("/books/:id", updatedBooks);
 };
