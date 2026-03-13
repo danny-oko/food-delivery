c5d6e3bdd674c3379e947a40402aa57e86282f2d
dnii <123633815+danny-oko@users.noreply.github.com>
2026-03-13 11:19:37 +0800

    some changes

--- src/controllers/foods/post-food.ts
+++ src/controllers/foods/post-food.ts
@@ -2,24 +2,6 @@ import { Context } from "hono";
 import { getDrizzleDb } from "../../db/db";
 import { Foodstable } from "../../db/schema/foods";
 
-// export const postFood = async (c: Context) => {
-//   const body = await c.req.json();
-//   const { name, description, price, category } = body;
-
-//   if (!name || price === undefined) {
-//     return c.json({ error: "name and price are required" }, 400);
-//   }
-//   const d1 = c.env.FOOD_DELIVERY;
-//   const db = getDrizzleDb(d1);
-
-//   const result = await db
-//     .insert(foodsTable)
-//     .values({ name, description, price, category })
-//     .returning();
-
-//   return c.json({ food: result[0] }, 201);
-// };
-
 export const postFood = async (c: Context) => {
   const body = await c.req.json();
   const { foodName, price, image, ingredients, categoryId } = body;
