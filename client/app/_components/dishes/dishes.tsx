e327a6e72810bd85adbc0cad1f1b8385cba4c895
danny <dnioko0412@gmail.com>
2026-04-04 16:12:27 +0800

    client home page foods map

--- client/app/_components/dishes/dishes.tsx
+++ client/app/_components/dishes/dishes.tsx
@@ -5,19 +5,22 @@ import type { Category } from "@/lib/types";
 export const Dishes = async () => {
   const { getAllCategories } = foodsService();
   const { results } = await getAllCategories();
+  // console.log(results);
+  const foods = await results.filter(
+    (category: Category) => category.foods.length > 0,
+  );
+
   return (
     <div className="min-h-screen bg-[#1c1c1c] px-4 py-6">
       <div className="max-w-[80%] mx-auto space-y-8">
-        {results
-          .filter((category: Category) => category.foods.length > 0)
-          .map((category: Category) => (
-            <section key={category.id}>
-              <h2 className="text-white text-xl font-semibold mb-4">
-                {category.name}
-              </h2>
-              <DishGrid foods={category.foods} />
-            </section>
-          ))}
+        {foods.map((category: Category) => (
+          <section key={category.id}>
+            <h2 className="text-white text-xl font-semibold mb-4">
+              {category.name}
+            </h2>
+            <DishGrid foods={category.foods} />
+          </section>
+        ))}
       </div>
     </div>
   );
